a149580d8e974b7dba40305b396ee55a515d6240
uniquepointer <uniquepointer@mailbox.org>
2021-09-29 23:38:29 -0500

    mid adding of plugins part 2

--- updateplugs.sh
+++ updateplugs.sh
@@ -4,4 +4,12 @@ git subtree pull --prefix mpy https://github.com/kammerdienerb/yed-paren_hl mast
 git subtree pull --prefix mpy https://github.com/kammerdienerb/yed-scroll_buffer master --squash;
 git subtree pull --prefix mpy https://github.com/kammerdienerb/yed-man master --squash;
 git subtree pull --prefix mpy https://github.com/kammerdienerb/yed-shell_run master --squash;
-git subtree pull --prefix mpy https://github.com/kammerdienerb/yed-status_bar_clock master --squash
+git subtree pull --prefix mpy https://github.com/kammerdienerb/yed-status_bar_clock master --squash;
+git subtree pull --prefix mpy https://github.com/kammerdienerb/yed-style_pack master --squash;
+git subtree pull --prefix mpy "https://github.com/kammerdienerb/yed-style_picker/" master --squash;
+git subtree pull --prefix mpy https://github.com/kammerdienerb/yed-style_use_term_bg master --squash;
+git subtree pull --prefix mpy https://github.com/kammerdienerb/yed-yedrc master --squash;
+git subtree pull --prefix mpy https://github.com/kammerdienerb/yed-calc master --squash;
+git subtree pull --prefix mpy https://github.com/kammerdienerb/yed-comment master --squash;
+git subtree pull --prefix mpy https://github.com/kammerdienerb/yed-completer master --squash;
+git subtree pull --prefix mpy https://github.com/kammerdienerb/yed-ctags master --squash;
