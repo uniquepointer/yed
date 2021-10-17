b009eb6531905a18252372e8386d6b3af31b7f3d
uniquepointer <uniquepointer@mailbox.org>
2021-10-17 17:27:59 -0500

    Squashed 'mpy/sysclip/' changes from 39a3614..d4496dc
    d4496dc Fixed logic errors
    e93b2b6 meowclip
    d7d12eb update sysclip to use threads
    
    git-subtree-dir: mpy/sysclip
    git-subtree-split: d4496dc2f52c9acd20e0951eb959f2b5866b698e

--- sysclip.c
+++ sysclip.c
@@ -1,9 +1,11 @@
 #include <yed/plugin.h>
 
-void
-sysclip(int n_args, char** args);
+void*
+sysclip();
 char*
 get_sel_text(yed_buffer* buffer);
+void
+thr_wrap(int n_args, char** args);
 
 int
 yed_plugin_boot(yed_plugin* self)
@@ -14,15 +16,45 @@ yed_plugin_boot(yed_plugin* self)
     {
         yed_set_var("sys-clip", "xsel");
     }
-    yed_plugin_set_command(self, "y2s", sysclip);
+    yed_plugin_set_command(self, "y2s", thr_wrap);
     return 0;
 }
 
 void
-sysclip(int n_args, char** args)
+thr_wrap(int n_args, char** args)
+{
+    yed_frame*  frame;
+    yed_buffer* buffer;
+    pthread_t   sctr;
+    int         tret;
+    if (!ys->active_frame)
+    {
+        yed_cerr("no active frame");
+        return;
+    }
+    frame = ys->active_frame;
+    if (!frame->buffer)
+    {
+        yed_cerr("active frame has no buffer");
+        return;
+    }
+
+    tret = pthread_create(&sctr, NULL, sysclip, NULL);
+    if (tret != 0)
+    {
+        yed_cerr("Failed to create thread");
+        return;
+    }
+}
+
+pthread_mutex_t scmtx = PTHREAD_MUTEX_INITIALIZER;
+void*
+sysclip()
 {
     yed_frame*  frame;
     yed_buffer* buff;
+
+    int  scmtx_state;
 
     int         output_len, status;
     char        cmd_buff[4096];
@@ -30,24 +62,28 @@ sysclip(int n_args, char** args)
     if (!ys->active_frame)
     {
         yed_cerr("no active frame");
-        return;
+        pthread_exit(NULL);
     }
-
     frame = ys->active_frame;
-
     if (!frame->buffer)
     {
         yed_cerr("active frame has no buffer");
-        return;
+        pthread_exit(NULL);
     }
-
     buff = frame->buffer;
-
     if (!buff->has_selection)
     {
         yed_cerr("nothing is selected");
-        return;
+        pthread_exit(NULL);
     }
+
+    scmtx_state = pthread_mutex_trylock(&scmtx);
+    if (scmtx_state != 0)
+    {
+        yed_cerr("Sysclip currently working.");
+        pthread_exit(NULL);
+    }
+
     char* str2paste = get_sel_text(frame->buffer);
     char* clip_pref   = yed_get_var("sys-clip");
     FILE* fp        = fopen("/tmp/.yedsysclipmeow", "w+");
@@ -58,12 +94,14 @@ sysclip(int n_args, char** args)
         i++;
     }
     fclose(fp);
-    snprintf(cmd_buff, sizeof(cmd_buff), "cat /tmp/.yedsysclipmeow | %s", clip_pref);
-
+    snprintf(cmd_buff, sizeof(cmd_buff), "cat /tmp/.yedsysclipmeow | %s",
+             clip_pref);
     yed_run_subproc(cmd_buff, &output_len, &status);
     yed_cprint("Yanked to system clipboard");
     remove("/tmp/.yedsysclipmeow");
     free(str2paste);
+    pthread_mutex_unlock(&scmtx);
+    pthread_exit(NULL);
 }
 
 /* ty kammer */
@@ -83,12 +121,9 @@ get_sel_text(yed_buffer* buffer)
     int       i;
     int       n;
     char*     data;
-
     nl    = '\n';
     chars = array_make(char);
-
     yed_range_sorted_points(&buffer->selection, &r1, &c1, &r2, &c2);
-
     if (buffer->selection.kind == RANGE_LINE)
     {
         for (r = r1; r <= r2; r += 1)
@@ -98,7 +133,6 @@ get_sel_text(yed_buffer* buffer)
             {
                 break;
             } /* should not happen */
-
             data = (char*)array_data(line->chars);
             array_push_n(chars, data, array_len(line->chars));
             array_push(chars, nl);
@@ -113,27 +147,21 @@ get_sel_text(yed_buffer* buffer)
             {
                 break;
             } /* should not happen */
-
             if (line->visual_width > 0)
             {
                 cstart = r == r1 ? c1 : 1;
                 cend   = r == r2 ? c2 : line->visual_width + 1;
-
                 i      = yed_line_col_to_idx(line, cstart);
                 n      = yed_line_col_to_idx(line, cend) - i;
                 data   = array_item(line->chars, i);
-
                 array_push_n(chars, data, n);
             }
-
             if (r < r2)
             {
                 array_push(chars, nl);
             }
         }
     }
-
     array_zero_term(chars);
-
     return (char*)array_data(chars);
 }
