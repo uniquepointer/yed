89dcb10f95d75ba3ef00f46e48ce9248f8888a31
uniquepointer <uniquepointer@mailbox.org>
2021-10-22 18:55:41 -0500

    Squashed 'mpy/lang/tools/clangfmt/' changes from e872f99..8400a75
    8400a75 this should not be used
    
    git-subtree-dir: mpy/lang/tools/clangfmt
    git-subtree-split: 8400a759952cc88748bbe3a6656c3a2cf6ef63c1

--- clangfmt.c
+++ clangfmt.c
@@ -1,10 +1,10 @@
 #include <yed/plugin.h>
 
 void
-buffer_clang_fmt(int n_args, char **args);
+buffer_clang_fmt(int n_args, char** args);
 
 void
-ev_buffer_clang_fang_fmt(yed_event *event);
+ev_buffer_clang_fmt(yed_event* event);
 
 void
 meow();
@@ -12,143 +12,123 @@ meow();
 char bufferLoc[512];
 
 int
-yed_plugin_boot(yed_plugin *self)
+yed_plugin_boot(yed_plugin* self)
 {
-  /*Check for plug version*/
-  YED_PLUG_VERSION_CHECK();
-
-  yed_event_handler eh_clang_format;
-
-  eh_clang_format.kind = EVENT_BUFFER_POST_WRITE;
-  eh_clang_format.fn   = ev_buffer_clang_fmt;
-
-  if (yed_get_var("clangfmt-auto") == NULL)
-  {
-    yed_set_var("clangfmt-auto", "yes");
-  }
-
-  if (yed_var_is_truthy("clangfmt-auto"))
-  {
-    yed_plugin_add_event_handler(self, eh_clang_format);
-  }
-
-  yed_plugin_set_command(self, "clang-fmt", buffer_clang_fmt);
-  return 0;
+    /*Check for plug version*/
+    YED_PLUG_VERSION_CHECK();
+    yed_event_handler eh_clang_format;
+    eh_clang_format.kind = EVENT_BUFFER_POST_WRITE;
+    eh_clang_format.fn   = ev_buffer_clang_fmt;
+    if (yed_get_var("clangfmt-auto") == NULL)
+    {
+        yed_set_var("clangfmt-auto", "yes");
+    }
+    if (yed_var_is_truthy("clangfmt-auto"))
+    {
+        yed_plugin_add_event_handler(self, eh_clang_format);
+    }
+    yed_plugin_set_command(self, "clang-fmt", buffer_clang_fmt);
+    return 0;
 }
 
 void
 buff_path_for_fmt()
 {
-  yed_buffer *buffer;
-  yed_frame * frame;
-
-  if (!ys->active_frame)
-  {
-    yed_cerr("no active frame");
-    return;
-  }
-
-  frame = ys->active_frame;
-
-  if (!frame->buffer)
-  {
-    yed_cerr("active frame has no buffer");
-    return;
-  }
-
-  buffer = frame->buffer;
-
-  if (buffer->name &&
-      (buffer->ft == yed_get_ft("C") || buffer->ft == yed_get_ft("C++")))
-  {
-    strcpy(bufferLoc, buffer->path);
-  }
-  else
-  {
-    yed_cerr("buffer has no path or it's not a C file'");
-  }
+    yed_buffer* buffer;
+    yed_frame* frame;
+    if (!ys->active_frame)
+    {
+        yed_cerr("no active frame");
+        return;
+    }
+    frame = ys->active_frame;
+    if (!frame->buffer)
+    {
+        yed_cerr("active frame has no buffer");
+        return;
+    }
+    buffer = frame->buffer;
+    if (buffer->name &&
+            (buffer->ft == yed_get_ft("C") || buffer->ft == yed_get_ft("C++")))
+    {
+        strcpy(bufferLoc, buffer->path);
+    }
+    else
+    {
+        yed_cerr("buffer has no path or it's not a C file'");
+    }
 }
 
 int
 _clang_fmt(void)
 {
-  int  output_len, status;
-  char cmd_buff[1024];
-
-  buff_path_for_fmt();
-  snprintf(cmd_buff, sizeof(cmd_buff), "clang-format -i %s &> /tmp/clangflog",
-           bufferLoc);
-  yed_run_subproc(cmd_buff, &output_len, &status);
-
-  if (status != 0)
-  {
-    yed_cerr("Failure to format C/C++\n");
-    return status;
-  }
-  else
-  {
-    yed_cprint("Formatted buffer\n");
-    return 0;
-  }
+    int  output_len, status;
+    char cmd_buff[1024];
+    buff_path_for_fmt();
+    snprintf(cmd_buff, sizeof(cmd_buff), "clang-format -i %s &> /tmp/clangflog",
+             bufferLoc);
+    yed_run_subproc(cmd_buff, &output_len, &status);
+    if (status != 0)
+    {
+        yed_cerr("Failure to format C/C++\n");
+        return status;
+    }
+    else
+    {
+        yed_cprint("Formatted buffer\n");
+        return 0;
+    }
 }
 void
-ev_buffer_clang_fmt(yed_event *event)
+ev_buffer_clang_fmt(yed_event* event)
 {
-  yed_frame *frame;
-
-  if (!ys->active_frame)
-  {
-    yed_cerr("no active frame");
-    return;
-  }
-
-  frame = ys->active_frame;
-
-  if (!frame->buffer)
-  {
-    yed_cerr("active frame has no buffer");
-    return;
-  }
-
-  if (frame->buffer->ft == yed_get_ft("C") ||
-      frame->buffer->ft == yed_get_ft("C++"))
-  {
-    if (_clang_fmt() == 0)
+    yed_frame* frame;
+    if (!ys->active_frame)
     {
-      YEXE("buffer-reload");
-      YEXE("redraw");
-      yed_cprint("Reloaded buffer\n");
+        yed_cerr("no active frame");
+        return;
     }
-  }
+    frame = ys->active_frame;
+    if (!frame->buffer)
+    {
+        yed_cerr("active frame has no buffer");
+        return;
+    }
+    if (frame->buffer->ft == yed_get_ft("C") ||
+            frame->buffer->ft == yed_get_ft("C++"))
+    {
+        if (_clang_fmt() == 0)
+        {
+            YEXE("buffer-reload");
+            YEXE("redraw");
+            yed_cprint("Reloaded buffer\n");
+        }
+    }
 }
 void
-buffer_clang_fmt(int n_args, char **args)
+buffer_clang_fmt(int n_args, char** args)
 {
-  yed_frame *frame;
-
-  if (!ys->active_frame)
-  {
-    yed_cerr("no active frame");
-    return;
-  }
-
-  frame = ys->active_frame;
-
-  if (!frame->buffer)
-  {
-    yed_cerr("active frame has no buffer");
-    return;
-  }
-
-  if (frame->buffer->ft == yed_get_ft("C") ||
-      frame->buffer->ft == yed_get_ft("C++"))
-  {
-    if (_clang_fmt() == 0)
+    yed_frame* frame;
+    if (!ys->active_frame)
     {
-      YEXE("write-buffer");
-      YEXE("buffer-reload");
-      YEXE("redraw");
-      yed_cprint("Reloaded buffer\n");
+        yed_cerr("no active frame");
+        return;
     }
-  }
+    frame = ys->active_frame;
+    if (!frame->buffer)
+    {
+        yed_cerr("active frame has no buffer");
+        return;
+    }
+    if (frame->buffer->ft == yed_get_ft("C") ||
+            frame->buffer->ft == yed_get_ft("C++"))
+    {
+        if (_clang_fmt() == 0)
+        {
+            YEXE("write-buffer");
+            YEXE("buffer-reload");
+            YEXE("redraw");
+            yed_cprint("Reloaded buffer\n");
+        }
+    }
 }
