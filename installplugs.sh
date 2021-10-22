b0246fb73b3b4e0215af1db02567b6114a73be83
uniquepointer <uniquepointer@mailbox.org>
2021-10-22 18:50:19 -0500

    fix plugs i think

--- installplugs.sh
+++ installplugs.sh
@@ -10,14 +10,14 @@ printf 'Copying lang/tools man pages\n';
 printf '%s\0' mpy/lang/tools/*/ | xargs -0 -L1 bash -c 'cd -- "$1" && cp *.7 ../../../../userconf/man/man7/.' _;
 
 printf 'Building plugins\n';
-printf '%s\0' mpy/*/ | xargs -0 -L1 bash -c 'cd -- "$1" && ./build.sh && cp *.so ../../userconf/plugins/.' _;
+printf '%s\0' mpy/*/ | xargs -0 -L1 bash -c 'cd -- "$1" && bash ./build.sh && cp *.so ../../userconf/plugins/.' _;
 printf 'Building lang plugins\n';
-printf '%s\0' mpy/lang/*/ | xargs -0 -L1 bash -c 'cd -- "$1" && ./build.sh && cp *.so ../../../userconf/plugins/lang/.' _;
+printf '%s\0' mpy/lang/*/ | xargs -0 -L1 bash -c 'cd -- "$1" && bash ./build.sh && cp *.so ../../../userconf/plugins/lang/.' _;
 printf 'Building style plugins\n'
-printf '%s\0' mpy/styles/*/ | xargs -0 -L1 bash -c 'cd -- "$1" && ./build.sh && cp *.so ../../../userconf/plugins/styles/.' _;
+printf '%s\0' mpy/styles/*/ | xargs -0 -L1 bash -c 'cd -- "$1" && bash ./build.sh && cp *.so ../../../userconf/plugins/styles/.' _;
 printf 'Building syntax plugins\n';
-printf '%s\0' mpy/lang/syntax/*/ | xargs -0 -L1 bash -c 'cd -- "$1" && ./build.sh && cp *.so ../../../../userconf/plugins/lang/syntax/.' _;
+printf '%s\0' mpy/lang/syntax/*/ | xargs -0 -L1 bash -c 'cd -- "$1" && bash ./build.sh && cp *.so ../../../../userconf/plugins/lang/syntax/.' _;
 printf 'Building lang tools plugins\n';
-printf '%s\0' mpy/lang/tools/*/ | xargs -0 -L1 bash -c 'cd -- "$1" && ./build.sh && cp *.so ../../../../userconf/plugins/lang/tools/.' _;
+printf '%s\0' mpy/lang/tools/*/ | xargs -0 -L1 bash -c 'cd -- "$1" && bash ./build.sh && cp *.so ../../../../userconf/plugins/lang/tools/.' _;
 
 printf 'Final touches...\n';
