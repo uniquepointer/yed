4db084adbbd5a8cbee60dc02860f7dcf11a73b54
uniquepointer <uniquepointer@mailbox.org>
2021-12-06 18:30:23 -0500

    Squashed 'mpy/lang/syntax/cpp/' changes from c1a4add..61c13cf
    61c13cf syntax.h
    
    git-subtree-dir: mpy/lang/syntax/cpp
    git-subtree-split: 61c13cfe2bbfa8c58846b19a65ff5678e175e630

--- mpy/lang/syntax/cpp/cpp.c
+++ mpy/lang/syntax/cpp/cpp.c
@@ -1,19 +1,69 @@
 #include <yed/plugin.h>
-#include <yed/highlight.h>
+#include <yed/syntax.h>
+
+static yed_syntax syn;
 
 #define ARRAY_LOOP(a) for (__typeof((a)[0]) *it = (a); it < (a) + (sizeof(a) / sizeof((a)[0])); ++it)
 
-highlight_info hinfo;
+#define _CHECK(x, r)                                                      \
+do {                                                                      \
+    if (x) {                                                              \
+        LOG_FN_ENTER();                                                   \
+        yed_log("[!] " __FILE__ ":%d regex error for '%s': %s", __LINE__, \
+                r,                                                        \
+                yed_syntax_get_regex_err(&syn));                          \
+        LOG_EXIT();                                                       \
+    }                                                                     \
+} while (0)
 
-void unload(yed_plugin *self);
-void syntax_c_line_handler(yed_event *event);
-void syntax_c_frame_handler(yed_event *event);
-void syntax_c_buff_mod_pre_handler(yed_event *event);
-void syntax_c_buff_mod_post_handler(yed_event *event);
+#define SYN()          yed_syntax_start(&syn)
+#define ENDSYN()       yed_syntax_end(&syn)
+#define APUSH(s)       yed_syntax_attr_push(&syn, s)
+#define APOP(s)        yed_syntax_attr_pop(&syn)
+#define RANGE(r)       _CHECK(yed_syntax_range_start(&syn, r), r)
+#define ONELINE()      yed_syntax_range_one_line(&syn)
+#define SKIP(r)        _CHECK(yed_syntax_range_skip(&syn, r), r)
+#define ENDRANGE(r)    _CHECK(yed_syntax_range_end(&syn, r), r)
+#define REGEX(r)       _CHECK(yed_syntax_regex(&syn, r), r)
+#define REGEXSUB(r, g) _CHECK(yed_syntax_regex_sub(&syn, r, g), r)
+#define KWD(k)         yed_syntax_kwd(&syn, k)
 
+#ifdef __APPLE__
+#define WB "[[:>:]]"
+#else
+#define WB "\\b"
+#endif
+
+void estyle(yed_event *event)   { yed_syntax_style_event(&syn, event);         }
+void ebuffdel(yed_event *event) { yed_syntax_buffer_delete_event(&syn, event); }
+void ebuffmod(yed_event *event) { yed_syntax_buffer_mod_event(&syn, event);    }
+void eline(yed_event *event)  {
+    yed_frame *frame;
+
+    frame = event->frame;
+
+    if (!frame
+    ||  !frame->buffer
+    ||  frame->buffer->kind != BUFF_KIND_FILE
+    ||  frame->buffer->ft != yed_get_ft("C++")) {
+        return;
+    }
+
+    yed_syntax_line_event(&syn, event);
+}
+
+
+void unload(yed_plugin *self) {
+    yed_syntax_free(&syn);
+    ys->redraw = 1;
+}
 
 int yed_plugin_boot(yed_plugin *self) {
-    yed_event_handler frame, line, buff_mod_pre, buff_mod_post;
+    yed_event_handler style;
+    yed_event_handler buffdel;
+    yed_event_handler buffmod;
+    yed_event_handler line;
+
     char              *kwds[] = {
         "__asm__",  "asm", "alignas", "alignof", "and", "and_eq", "atomic_cancel", "atomic_commit", "atomic_noexcept", "auto",
         "bitand", "bitor",
@@ -33,15 +83,6 @@ int yed_plugin_boot(yed_plugin *self) {
         "union", "using",
         "virtual", "volatile",
         "xor", "xor_eq",
-    };
-    char              *pp_kwds[] = {
-        "define",
-        "elif", "else", "endif", "error",
-        "if", "ifdef", "ifndef", "include",
-        "message",
-        "pragma",
-        "undef",
-        "warning",
     };
     char              *control_flow[] = {
         "break",
@@ -69,122 +110,112 @@ int yed_plugin_boot(yed_plugin *self) {
         "wchar_t",
     };
 
+
     YED_PLUG_VERSION_CHECK();
 
     yed_plugin_set_unload_fn(self, unload);
 
+    style.kind = EVENT_STYLE_CHANGE;
+    style.fn   = estyle;
+    yed_plugin_add_event_handler(self, style);
 
-    frame.kind          = EVENT_FRAME_PRE_BUFF_DRAW;
-    frame.fn            = syntax_c_frame_handler;
-    line.kind           = EVENT_LINE_PRE_DRAW;
-    line.fn             = syntax_c_line_handler;
-    buff_mod_pre.kind   = EVENT_BUFFER_PRE_MOD;
-    buff_mod_pre.fn     = syntax_c_buff_mod_pre_handler;
-    buff_mod_post.kind  = EVENT_BUFFER_POST_MOD;
-    buff_mod_post.fn    = syntax_c_buff_mod_post_handler;
+    buffdel.kind = EVENT_BUFFER_PRE_DELETE;
+    buffdel.fn   = ebuffdel;
+    yed_plugin_add_event_handler(self, buffdel);
 
-    yed_plugin_add_event_handler(self, frame);
+    buffmod.kind = EVENT_BUFFER_POST_MOD;
+    buffmod.fn   = ebuffmod;
+    yed_plugin_add_event_handler(self, buffmod);
+
+    line.kind = EVENT_LINE_PRE_DRAW;
+    line.fn   = eline;
     yed_plugin_add_event_handler(self, line);
-    yed_plugin_add_event_handler(self, buff_mod_pre);
-    yed_plugin_add_event_handler(self, buff_mod_post);
 
 
-    highlight_info_make(&hinfo);
+    SYN();
+        APUSH("&code-comment");
+            RANGE("/\\*");
+            ENDRANGE(  "\\*/");
+            RANGE("//");
+            ENDRANGE("$");
+            RANGE("^[[:space:]]*#[[:space:]]*if[[:space:]]+0"WB);
+            ENDRANGE("^[[:space:]]*#[[:space:]]*(else|endif|elif|elifdef)"WB);
+        APOP();
 
-    ARRAY_LOOP(kwds)
-        highlight_add_kwd(&hinfo, *it, HL_KEY);
-    ARRAY_LOOP(pp_kwds)
-        highlight_add_prefixed_kwd(&hinfo, '#', *it, HL_PP);
-    ARRAY_LOOP(control_flow)
-        highlight_add_kwd(&hinfo, *it, HL_CF);
-    ARRAY_LOOP(typenames)
-        highlight_add_kwd(&hinfo, *it, HL_TYPE);
-    highlight_add_kwd(&hinfo, "__VA_ARGS__", HL_PP);
-    highlight_add_kwd(&hinfo, "__FILE__", HL_PP);
-    highlight_add_kwd(&hinfo, "__func__", HL_PP);
-    highlight_add_kwd(&hinfo, "__FUNCTION__", HL_PP);
-    highlight_add_kwd(&hinfo, "__LINE__", HL_PP);
-    highlight_add_kwd(&hinfo, "__DATE__", HL_PP);
-    highlight_add_kwd(&hinfo, "__TIME__", HL_PP);
-    highlight_add_kwd(&hinfo, "__STDC__", HL_PP);
-    highlight_add_kwd(&hinfo, "__STDC_VERSION__", HL_PP);
-    highlight_add_kwd(&hinfo, "__STDC_HOSTED__", HL_PP);
-    highlight_add_kwd(&hinfo, "__cplusplus", HL_PP);
-    highlight_add_kwd(&hinfo, "__OBJC__", HL_PP);
-    highlight_add_kwd(&hinfo, "__ASSEMBLER__", HL_PP);
-    highlight_add_kwd(&hinfo, "NULL", HL_CON);
-    highlight_add_kwd(&hinfo, "stdin", HL_CON);
-    highlight_add_kwd(&hinfo, "stdout", HL_CON);
-    highlight_add_kwd(&hinfo, "stderr", HL_CON);
-    highlight_add_kwd(&hinfo, "false", HL_CON);
-    highlight_add_kwd(&hinfo, "true", HL_CON);
-    highlight_add_kwd(&hinfo, "nullptr", HL_CON);
-    highlight_add_kwd(&hinfo, "this", HL_CON);
+        APUSH("&code-string");
+            REGEX("'(\\\\.|[^'\\\\])'");
 
-    highlight_suffixed_words(&hinfo, '(', HL_CALL);
-    highlight_numbers(&hinfo);
-    highlight_within(&hinfo, "\"", "\"", '\\', -1, HL_STR);
-    highlight_within(&hinfo, "'", "'", '\\', 1, HL_CHAR);
-    highlight_to_eol_from(&hinfo, "//", HL_COMMENT);
-    highlight_within_multiline(&hinfo, "/*", "*/", 0, HL_COMMENT);
-    highlight_within_multiline(&hinfo, "#if 0", "#endif", 0, HL_COMMENT);
+            RANGE("\""); ONELINE(); SKIP("\\\\\"");
+                APUSH("&code-escape");
+                    REGEX("\\\\.");
+                APOP();
+            ENDRANGE("\"");
+        APOP();
+
+        APUSH("&code-fn-call");
+            REGEXSUB("([[:alpha:]_][[:alnum:]_]*)[[:space:]]*\\(", 1);
+        APOP();
+
+        APUSH("&code-number");
+            REGEXSUB("(^|[^[:alnum:]_])(-?([[:digit:]]+\\.[[:digit:]]*)|(([[:digit:]]*\\.[[:digit:]]+))(e\\+[[:digit:]]+)?[fFlL]?)"WB, 2);
+            REGEXSUB("(^|[^[:alnum:]_])(-?[[:digit:]]+(([uU]?[lL]{0,2})|([lL]{0,2}[uU]?))?)"WB, 2);
+            REGEXSUB("(^|[^[:alnum:]_])(0[xX][0-9a-fA-F]+(([uU]?[lL]{0,2})|([lL]{0,2}[uU]?))?)"WB, 2);
+        APOP();
+
+        APUSH("&code-keyword");
+            ARRAY_LOOP(kwds) KWD(*it);
+        APOP();
+
+        APUSH("&code-control-flow");
+            ARRAY_LOOP(control_flow) KWD(*it);
+            REGEXSUB("^[[:space:]]*([[:alpha:]_][[:alnum:]_]*):", 1);
+        APOP();
+
+        APUSH("&code-typename");
+            ARRAY_LOOP(typenames) KWD(*it);
+        APOP();
+
+        APUSH("&code-preprocessor");
+            KWD("__VA_ARGS__");
+            KWD("__FILE__");
+            KWD("__func__");
+            KWD("__FUNCTION__");
+            KWD("__LINE__");
+            KWD("__DATE__");
+            KWD("__TIME__");
+            KWD("__STDC__");
+            KWD("__STDC_VERSION__");
+            KWD("__STDC_HOSTED__");
+            KWD("__cplusplus");
+            KWD("__OBJC__");
+            KWD("__ASSEMBLER__");
+
+            RANGE("^[[:space:]]*#[[:space:]]*include"); ONELINE();
+                APUSH("&code-string");
+                    REGEX("[<\"].*");
+                APOP();
+            ENDRANGE("$");
+
+            REGEX("^[[:space:]]*#[[:space:]]*(define|elif|else|endif|error|if|ifdef|ifndef|line|message|pragma|undef|warning)"WB);
+        APOP();
+
+        APUSH("&code-constant");
+            KWD("NULL");
+            KWD("stdin");
+            KWD("stdout");
+            KWD("stderr");
+            KWD("true");
+            KWD("false");
+            KWD("nullptr");
+            KWD("this");
+        APOP();
+
+        APUSH("&code-field");
+            REGEXSUB("(\\.|->)[[:space:]]*([[:alpha:]_][[:alnum:]_]*)", 2);
+        APOP();
+    ENDSYN();
 
     ys->redraw = 1;
 
     return 0;
-}
-
-void unload(yed_plugin *self) {
-    highlight_info_free(&hinfo);
-    ys->redraw = 1;
-}
-
-void syntax_c_frame_handler(yed_event *event) {
-    yed_frame *frame;
-
-    frame = event->frame;
-
-    if (!frame
-    ||  !frame->buffer
-    ||  frame->buffer->kind != BUFF_KIND_FILE
-    ||  frame->buffer->ft != yed_get_ft("C++")) {
-        return;
-    }
-
-    highlight_frame_pre_draw_update(&hinfo, event);
-}
-
-void syntax_c_line_handler(yed_event *event) {
-    yed_frame *frame;
-
-    frame = event->frame;
-
-    if (!frame
-    ||  !frame->buffer
-    ||  frame->buffer->kind != BUFF_KIND_FILE
-    ||  frame->buffer->ft != yed_get_ft("C++")) {
-        return;
-    }
-
-    highlight_line(&hinfo, event);
-}
-
-void syntax_c_buff_mod_pre_handler(yed_event *event) {
-    if (event->buffer == NULL
-    ||  event->buffer->kind != BUFF_KIND_FILE
-    ||  event->buffer->ft != yed_get_ft("C++")) {
-        return;
-    }
-
-    highlight_buffer_pre_mod_update(&hinfo, event);
-}
-
-void syntax_c_buff_mod_post_handler(yed_event *event) {
-    if (event->buffer == NULL
-    ||  event->buffer->kind != BUFF_KIND_FILE
-    ||  event->buffer->ft != yed_get_ft("C++")) {
-        return;
-    }
-
-    highlight_buffer_post_mod_update(&hinfo, event);
 }
