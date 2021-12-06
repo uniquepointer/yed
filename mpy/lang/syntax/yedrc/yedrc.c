0096eb29b3f9ac20c24fa4a5478486b046e8a7cf
uniquepointer <uniquepointer@mailbox.org>
2021-12-06 18:30:39 -0500

    Squashed 'mpy/lang/syntax/yedrc/' changes from 8758231..7dbbfc5
    7dbbfc5 syntax.h
    
    git-subtree-dir: mpy/lang/syntax/yedrc
    git-subtree-split: 7dbbfc502d850f2e6c54ff2686ccdbb701fd329e

--- mpy/lang/syntax/yedrc/yedrc.c
+++ mpy/lang/syntax/yedrc/yedrc.c
@@ -1,45 +1,45 @@
 #include <yed/plugin.h>
-#include <yed/highlight.h>
+#include <yed/syntax.h>
 
-void unload(yed_plugin *self);
-void syntax_yedrc_line_handler(yed_event *event);
+static yed_syntax syn;
+
+
+#define _CHECK(x, r)                                                      \
+do {                                                                      \
+    if (x) {                                                              \
+        LOG_FN_ENTER();                                                   \
+        yed_log("[!] " __FILE__ ":%d regex error for '%s': %s", __LINE__, \
+                r,                                                        \
+                yed_syntax_get_regex_err(&syn));                          \
+        LOG_EXIT();                                                       \
+    }                                                                     \
+} while (0)
+
+#define SYN()          yed_syntax_start(&syn)
+#define ENDSYN()       yed_syntax_end(&syn)
+#define APUSH(s)       yed_syntax_attr_push(&syn, s)
+#define APOP(s)        yed_syntax_attr_pop(&syn)
+#define RANGE(r)       _CHECK(yed_syntax_range_start(&syn, r), r)
+#define ONELINE()      yed_syntax_range_one_line(&syn)
+#define SKIP(r)        _CHECK(yed_syntax_range_skip(&syn, r), r)
+#define ENDRANGE(r)    _CHECK(yed_syntax_range_end(&syn, r), r)
+#define REGEX(r)       _CHECK(yed_syntax_regex(&syn, r), r)
+#define REGEXSUB(r, g) _CHECK(yed_syntax_regex_sub(&syn, r, g), r)
+#define KWD(k)         yed_syntax_kwd(&syn, k)
+
+#ifdef __APPLE__
+#define WB "[[:>:]]"
+#else
+#define WB "\\b"
+#endif
+
+
 void syntax_yedrc_highlight(yed_event *event);
 
-highlight_info comment_hinfo;
-highlight_info strings_and_numbers_hinfo;
-
-int yed_plugin_boot(yed_plugin *self) {
-    yed_event_handler h;
-
-    YED_PLUG_VERSION_CHECK();
-
-    yed_plugin_set_unload_fn(self, unload);
-
-    h.kind = EVENT_LINE_PRE_DRAW;
-    h.fn   = syntax_yedrc_line_handler;
-
-    yed_plugin_add_event_handler(self, h);
-
-    highlight_info_make(&comment_hinfo);
-    highlight_info_make(&strings_and_numbers_hinfo);
-
-    highlight_to_eol_from(&comment_hinfo, "#", HL_COMMENT);
-    highlight_numbers(&strings_and_numbers_hinfo);
-    highlight_within(&strings_and_numbers_hinfo, "\"", "\"", '\\', -1, HL_STR);
-    highlight_within(&strings_and_numbers_hinfo, "'", "'", '\\', -1, HL_STR);
-
-    ys->redraw = 1;
-
-    return 0;
-}
-
-void unload(yed_plugin *self) {
-    highlight_info_free(&strings_and_numbers_hinfo);
-    highlight_info_free(&comment_hinfo);
-    ys->redraw = 1;
-}
-
-void syntax_yedrc_line_handler(yed_event *event) {
+void estyle(yed_event *event)   { yed_syntax_style_event(&syn, event);         }
+void ebuffdel(yed_event *event) { yed_syntax_buffer_delete_event(&syn, event); }
+void ebuffmod(yed_event *event) { yed_syntax_buffer_mod_event(&syn, event);    }
+void eline(yed_event *event)  {
     yed_frame *frame;
 
     frame = event->frame;
@@ -51,9 +51,63 @@ void syntax_yedrc_line_handler(yed_event *event) {
         return;
     }
 
-    highlight_line(&strings_and_numbers_hinfo, event);
     syntax_yedrc_highlight(event);
-    highlight_line(&comment_hinfo, event);
+    yed_syntax_line_event(&syn, event);
+}
+
+
+void unload(yed_plugin *self) {
+    yed_syntax_free(&syn);
+    ys->redraw = 1;
+}
+
+int yed_plugin_boot(yed_plugin *self) {
+    yed_event_handler style;
+    yed_event_handler buffdel;
+    yed_event_handler buffmod;
+    yed_event_handler line;
+
+
+    YED_PLUG_VERSION_CHECK();
+
+    yed_plugin_set_unload_fn(self, unload);
+
+    style.kind = EVENT_STYLE_CHANGE;
+    style.fn   = estyle;
+    yed_plugin_add_event_handler(self, style);
+
+    buffdel.kind = EVENT_BUFFER_PRE_DELETE;
+    buffdel.fn   = ebuffdel;
+    yed_plugin_add_event_handler(self, buffdel);
+
+    buffmod.kind = EVENT_BUFFER_POST_MOD;
+    buffmod.fn   = ebuffmod;
+    yed_plugin_add_event_handler(self, buffmod);
+
+    line.kind = EVENT_LINE_PRE_DRAW;
+    line.fn   = eline;
+    yed_plugin_add_event_handler(self, line);
+
+
+    SYN();
+        APUSH("&code-comment");
+            RANGE("#"); ONELINE(); ENDRANGE("$");
+        APOP();
+
+        APUSH("&code-string");
+            RANGE("\""); SKIP("\\\\\""); ENDRANGE("\"");
+            RANGE("'");  SKIP("\\\\'");  ENDRANGE("'");
+        APOP();
+
+        APUSH("&code-number");
+            REGEXSUB("(^|[^[:alnum:]_])(-?([[:digit:]]+\\.[[:digit:]]*)|(([[:digit:]]*\\.[[:digit:]]+)))"WB, 2);
+            REGEXSUB("(^|[^[:alnum:]_])(-?[[:digit:]]+)"WB, 2);
+        APOP();
+    ENDSYN();
+
+    ys->redraw = 1;
+
+    return 0;
 }
 
 void syntax_yedrc_highlight(yed_event *event) {
@@ -81,7 +135,7 @@ void syntax_yedrc_highlight(yed_event *event) {
 
         if (c == ')') { return; }
 
-        if (isalnum(c) || c == '_' || c == '-') {
+        if (is_alnum(c) || c == '_' || c == '-') {
             while (col <= line->visual_width) {
                 word_len += 1;
                 col      += 1;
@@ -90,7 +144,7 @@ void syntax_yedrc_highlight(yed_event *event) {
 
                 c = yed_line_col_to_glyph(line, col)->c;
 
-                if (!isalnum(c) && c != '_' && c != '-') {
+                if (!is_alnum(c) && c != '_' && c != '-') {
                     break;
                 }
             }
@@ -103,13 +157,13 @@ void syntax_yedrc_highlight(yed_event *event) {
 
                 c = yed_line_col_to_glyph(line, col)->c;
 
-                if (isalnum(c) || c == '_' || c == '-' || isspace(c)) {
+                if (is_alnum(c) || c == '_' || c == '-' || is_space(c)) {
                     break;
                 }
             }
         }
 
-        if (isspace(c)) {
+        if (is_space(c)) {
             while (col <= line->visual_width) {
                 col += 1;
 
@@ -117,7 +171,7 @@ void syntax_yedrc_highlight(yed_event *event) {
 
                 c = yed_line_col_to_glyph(line, col)->c;
 
-                if (!isspace(c)) { break; }
+                if (!is_space(c)) { break; }
             }
         }
 
