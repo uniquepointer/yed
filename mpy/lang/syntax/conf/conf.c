ae4d089012cb682f6dc146375141a0d0514091b2
uniquepointer <uniquepointer@mailbox.org>
2021-12-06 18:30:16 -0500

    Squashed 'mpy/lang/syntax/conf/' changes from 3893ee4..8e56c15
    8e56c15 syntax.h
    
    git-subtree-dir: mpy/lang/syntax/conf
    git-subtree-split: 8e56c15ff6a58161ce7b23b1bda7af9fe960a86b

--- mpy/lang/syntax/conf/conf.c
+++ mpy/lang/syntax/conf/conf.c
@@ -1,41 +1,42 @@
 #include <yed/plugin.h>
-#include <yed/highlight.h>
+#include <yed/syntax.h>
 
-void unload(yed_plugin *self);
-void syntax_conf_line_handler(yed_event *event);
+static yed_syntax syn;
 
-highlight_info hinfo;
 
-int yed_plugin_boot(yed_plugin *self) {
-    yed_event_handler h;
+#define _CHECK(x, r)                                                      \
+do {                                                                      \
+    if (x) {                                                              \
+        LOG_FN_ENTER();                                                   \
+        yed_log("[!] " __FILE__ ":%d regex error for '%s': %s", __LINE__, \
+                r,                                                        \
+                yed_syntax_get_regex_err(&syn));                          \
+        LOG_EXIT();                                                       \
+    }                                                                     \
+} while (0)
 
-    YED_PLUG_VERSION_CHECK();
+#define SYN()          yed_syntax_start(&syn)
+#define ENDSYN()       yed_syntax_end(&syn)
+#define APUSH(s)       yed_syntax_attr_push(&syn, s)
+#define APOP(s)        yed_syntax_attr_pop(&syn)
+#define RANGE(r)       _CHECK(yed_syntax_range_start(&syn, r), r)
+#define ONELINE()      yed_syntax_range_one_line(&syn)
+#define SKIP(r)        _CHECK(yed_syntax_range_skip(&syn, r), r)
+#define ENDRANGE(r)    _CHECK(yed_syntax_range_end(&syn, r), r)
+#define REGEX(r)       _CHECK(yed_syntax_regex(&syn, r), r)
+#define REGEXSUB(r, g) _CHECK(yed_syntax_regex_sub(&syn, r, g), r)
+#define KWD(k)         yed_syntax_kwd(&syn, k)
 
-    yed_plugin_set_unload_fn(self, unload);
+#ifdef __APPLE__
+#define WB "[[:>:]]"
+#else
+#define WB "\\b"
+#endif
 
-    h.kind = EVENT_LINE_PRE_DRAW;
-    h.fn   = syntax_conf_line_handler;
-
-    yed_plugin_add_event_handler(self, h);
-
-    highlight_info_make(&hinfo);
-
-    highlight_to_eol_from(&hinfo, "#", HL_COMMENT);
-    highlight_numbers(&hinfo);
-    highlight_within(&hinfo, "\"", "\"", '\\', -1, HL_STR);
-    highlight_within(&hinfo, "'", "'", '\\', -1, HL_STR);
-
-    ys->redraw = 1;
-
-    return 0;
-}
-
-void unload(yed_plugin *self) {
-    highlight_info_free(&hinfo);
-    ys->redraw = 1;
-}
-
-void syntax_conf_line_handler(yed_event *event) {
+void estyle(yed_event *event)   { yed_syntax_style_event(&syn, event);         }
+void ebuffdel(yed_event *event) { yed_syntax_buffer_delete_event(&syn, event); }
+void ebuffmod(yed_event *event) { yed_syntax_buffer_mod_event(&syn, event);    }
+void eline(yed_event *event)  {
     yed_frame *frame;
 
     frame = event->frame;
@@ -47,5 +48,62 @@ void syntax_conf_line_handler(yed_event *event) {
         return;
     }
 
-    highlight_line(&hinfo, event);
+    yed_syntax_line_event(&syn, event);
+}
+
+
+void unload(yed_plugin *self) {
+    yed_syntax_free(&syn);
+    ys->redraw = 1;
+}
+
+int yed_plugin_boot(yed_plugin *self) {
+    yed_event_handler style;
+    yed_event_handler buffdel;
+    yed_event_handler buffmod;
+    yed_event_handler line;
+
+
+    YED_PLUG_VERSION_CHECK();
+
+    yed_plugin_set_unload_fn(self, unload);
+
+    style.kind = EVENT_STYLE_CHANGE;
+    style.fn   = estyle;
+    yed_plugin_add_event_handler(self, style);
+
+    buffdel.kind = EVENT_BUFFER_PRE_DELETE;
+    buffdel.fn   = ebuffdel;
+    yed_plugin_add_event_handler(self, buffdel);
+
+    buffmod.kind = EVENT_BUFFER_POST_MOD;
+    buffmod.fn   = ebuffmod;
+    yed_plugin_add_event_handler(self, buffmod);
+
+    line.kind = EVENT_LINE_PRE_DRAW;
+    line.fn   = eline;
+    yed_plugin_add_event_handler(self, line);
+
+
+    SYN();
+        APUSH("&code-comment");
+            RANGE("#"); ONELINE();
+            ENDRANGE("$");
+        APOP();
+
+        APUSH("&code-string");
+            RANGE("\""); SKIP("\\\\\""); ENDRANGE("\"");
+            RANGE("'");  SKIP("\\\\'");  ENDRANGE("'");
+        APOP();
+
+        APUSH("&code-number");
+            REGEXSUB("(^|[^[:alnum:]_])(-?([[:digit:]]+\\.[[:digit:]]*)|(([[:digit:]]*\\.[[:digit:]]+)))"WB, 2);
+            REGEXSUB("(^|[^[:alnum:]_])(-?[[:digit:]]+)"WB, 2);
+            REGEXSUB("(^|[^[:alnum:]_])(0[xX][0-9a-fA-F]+)"WB, 2);
+        APOP();
+    ENDSYN();
+
+    ys->redraw = 1;
+
+    return 0;
 }
